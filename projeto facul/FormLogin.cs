2d580305e4b1327c7462b68080b997728849b1f4
luuizzfelipe <luiznascimento95@hotmail.com>
2020-10-30 00:22:57 -0300

    Adicionando CRUD-v.1

---
+++ projeto facul/FormLogin.cs
@@ -0,0 +1,37 @@
+﻿using projeto_facul.DAO;
+using projeto_facul.MODELS;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace projeto_facul
+{
+    public partial class FormLogin : Form
+    {
+        public FormLogin()
+        {
+            InitializeComponent();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void btnLogin_Click(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void gunaTextBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
